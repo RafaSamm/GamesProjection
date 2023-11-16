f58ecc2aa609b0f4c18e8ff00a9a1123f64594a9
RafaSamm <rafarhs286@gmail.com>
2023-11-16 08:44:21 -0300

    Project created

---
+++ src/main/java/br/com/rhssolutions/GamesProjection/GamesProjectionApplication.java
@@ -0,0 +1,13 @@
+package br.com.rhssolutions.GamesProjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesProjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesProjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rhssolutions/GamesProjection/GamesProjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rhssolutions.GamesProjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesProjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
